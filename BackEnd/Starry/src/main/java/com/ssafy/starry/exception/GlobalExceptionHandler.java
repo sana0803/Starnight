34d426ae7962b2738e12fc4cdd7be617f8e4982e
KJH-0105 <juhyun7955@naver.com>
2021-10-07 15:29:02 +0900

    fix : testCode fix

--- BackEnd/Starry/src/main/java/com/ssafy/starry/exception/GlobalExceptionHandler.java
+++ BackEnd/Starry/src/main/java/com/ssafy/starry/exception/GlobalExceptionHandler.java
@@ -12,6 +12,7 @@ import com.ssafy.starry.exception.valid.ForbiddenWordException;
 import com.ssafy.starry.exception.valid.WordNotValidException;
 import java.util.Enumeration;
 import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,10 +24,10 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @Slf4j
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @Autowired
-    private NotificationManager notificationManager;
+    private final NotificationManager notificationManager;
 
     /*
     금지된 단어가 입력되었을 때 발생하는 Exception _ searchWords
@@ -34,7 +35,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ForbiddenWordException.class)
     public ResponseEntity<String> handleForbiddenWordException(
         ForbiddenWordException ex, HttpServletRequest req
-    ){
+    ) {
         log.debug("Forbidden word: searchWords", ex);
         notificationManager.sendNotification(ex, req.getRequestURI(), getParams(req));
         return FORBIDDEN;
@@ -46,7 +47,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(WordNotValidException.class)
     public ResponseEntity<String> handleForbiddenWordException(
         WordNotValidException ex, HttpServletRequest req
-    ){
+    ) {
         log.debug("Word not valid: not null && 1 <= word.length <= 10", ex);
         notificationManager.sendNotification(ex, req.getRequestURI(), getParams(req));
         return WORD_NOT_VALID;
@@ -58,7 +59,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(DataTrendNullPointerException.class)
     public ResponseEntity<String> handleDataTrendNullPointerException(
         DataTrendNullPointerException ex, HttpServletRequest req
-    ){
+    ) {
         log.debug("API connection failed: LIST_NPE", ex);
         notificationManager.sendNotification(ex, req.getRequestURI(), getParams(req));
         return DATA_TREND_NPE;
@@ -70,7 +71,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ListNullPointerException.class)
     public ResponseEntity<String> handleListNullPointerException(
         ListNullPointerException ex, HttpServletRequest req
-    ){
+    ) {
         log.debug("API connection failed: DATA_TREND_NPE", ex);
         notificationManager.sendNotification(ex, req.getRequestURI(), getParams(req));
         return LIST_NPE;
@@ -78,7 +79,8 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     // 5xx error handler : 서버에서 발생한 전반적인 에러에 대한 핸들러
     @ExceptionHandler(Exception.class)
-    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request, HttpServletRequest req) {
+    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request,
+        HttpServletRequest req) {
         ExceptionResponse exceptionResponse = ExceptionResponse.builder()
             .Error(request.getDescription(false))
             .message(ex.getMessage())
@@ -93,7 +95,8 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         Enumeration<String> keys = req.getParameterNames();
         while (keys.hasMoreElements()) {
             String key = keys.nextElement();
-            params.append("- ").append(key).append(" : ").append(req.getParameter(key)).append('\n');
+            params.append("- ").append(key).append(" : ").append(req.getParameter(key))
+                .append('\n');
         }
 
         return params.toString();
